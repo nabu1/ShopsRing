061ae3c3ef018579112ae8c6b73acd3bc659805a
gyubal <nabu1312@gmail.com>
2019-01-02 19:37:31 +0100

    StockSelector już działa ok

--- src/components/StockSelector/StockSelector.vue
+++ src/components/StockSelector/StockSelector.vue
@@ -1,59 +1,50 @@
 <template>
   <div>
     <b-container>
-      <b-row class='klasa'>
-
+      <b-row class="klasa">
         <b-form-group label="Pieczywo i nabiał">
-          <b-form-checkbox-group v-model="selected1"
-                                 :options="options1">
-          </b-form-checkbox-group>
+          <b-form-checkbox-group v-model="selected1" :options="options1"></b-form-checkbox-group>
         </b-form-group>
       </b-row>
 
-      <b-row class='klasa'>
+      <b-row class="klasa">
         <b-form-group label="Mięcho i wędliny">
-          <b-form-checkbox-group v-model="selected2"
-                                 :options="options2">
-          </b-form-checkbox-group>
+          <b-form-checkbox-group v-model="selected2" :options="options2"></b-form-checkbox-group>
         </b-form-group>
       </b-row>
 
-      <b-row class='klasa'>
-        Selected: {{ selected1.concat(selected2).toString() }}
+      <b-row class="klasa">Selected: {{ selected1.concat(selected2).toString() }}</b-row>
+
+      <b-row class="klasa">
+        <button @click="stocksSelected">OK</button>
       </b-row>
-
-      <b-row class='klasa'>
-        <button @click='stocksSelected'>OK</button>
-      </b-row>
-
-
     </b-container>
   </div>
 </template>
 
 <script>
-  import { stocks1, stocks2 } from '../../data/data'
+import { stocks1, stocks2 } from "../../data/data"
 
-  export default {
-    data() {
-      return {
-        selected1: [],
-        selected2: [],
-        options1: stocks1,
-        options2: stocks2
-      }
-    },
-    methods: {
-      stocksSelected() {
-        console.log(this.selected1.concat(this.selected2))
-      }
+export default {
+  data() {
+    return {
+      selected1: [],
+      selected2: [],
+      options1: stocks1,
+      options2: stocks2
+    };
+  },
+  methods: {
+    stocksSelected() {
+      const stocksSelected = this.selected1.concat(this.selected2)
+      this.$store.dispatch("getFields", stocksSelected)
     }
   }
+}
 </script>
 
 <style scoped>
-  .klasa {
-    font-weight: 600
-  }
-
+.klasa {
+  font-weight: 600
+}
 </style>
