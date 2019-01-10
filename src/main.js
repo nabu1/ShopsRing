82ef4cbcb705268aa3cd5c969b32c17d607020c1
gyubal <nabu1312@gmail.com>
2019-01-10 16:49:52 +0100

    WiP - routing do About chodzi

--- src/main.js
+++ src/main.js
@@ -12,12 +12,13 @@ Vue.use(BootstrapVue)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+  routes,
+  mode: 'history'
 })
 
 new Vue({
   el: '#app',
   store,
+  router,
   render: h => h(App)
 })
