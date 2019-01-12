65b8c0e1e42859b922bb4e6400f98d85bf44c934
gyubal <nabu1312@gmail.com>
2019-01-12 15:33:07 +0100

    Done - 10 testów cypress

--- cypress/integration/examples/newTest.spec.js
+++ cypress/integration/examples/newTest.spec.js
@@ -57,17 +57,29 @@ Lista testów e2e:
 describe('Searching shops', () => {
   beforeEach(() => {
     cy.visit('/')
+    .get('#city')
+    .select("Warszawa")
+    .get('#street')
+    .type('Dolna')
+    .get('#streetNumber')
+    .type('5a')
   })
   //cy.pause()
 
-  it('Enter home location and check number of shops returned', () => {
-    cy.get('#city')
-      .select("Warszawa")
-      .get('#street')
-      .type('Dolna')
-      .get('#streetNumber')
-      .type('5a')
-      .get('#radius')
+  it('1. Enter Dolna 5a, radius = 100 and test if 1 shop were returned', () => {
+    cy.get('#radius')
+      .clear()
+      .type(100)
+      .get('#search')
+      .click()
+      .get('#stockTable')
+      .find('tbody')
+      .find('tr')
+      .should('have.length', 1)
+  })
+
+  it('2. Enter Dolna 5a, radius = 500 and test if 3 shops were returned', () => {
+    cy.get('#radius')
       .clear()
       .type(500)
       .get('#search')
@@ -77,6 +89,103 @@ describe('Searching shops', () => {
       .find('tr')
       .should('have.length', 3)
   })
+
+  it('3. Enter Dolna 5a, radius = 600 and test if 5 shops were returned', () => {
+    cy.get('#radius')
+      .clear()
+      .type(600)
+      .get('#search')
+      .click()
+      .get('#stockTable')
+      .find('tbody')
+      .find('tr')
+      .should('have.length', 5)
+  })
+
+  it('4. Enter Dolna 5a, radius = 800 and test if 9 shops were returned', () => {
+    cy.get('#radius')
+      .clear()
+      .type(800)
+      .get('#search')
+      .click()
+      .get('#stockTable')
+      .find('tbody')
+      .find('tr')
+      .should('have.length', 9)
+  })
+
+  it('5. Enter Dolna 5a, radius = 1000 and test if 14 shops were returned', () => {
+    cy.get('#radius')
+      .clear()
+      .type(1000)
+      .get('#search')
+      .click()
+      .get('#stockTable')
+      .find('tbody')
+      .find('tr')
+      .should('have.length', 14)
+  })
+
+  it('6. Enter Dolna 5a, radius = 1100 and test if 17 shops were returned', () => {
+    cy.get('#radius')
+      .clear()
+      .type(1100)
+      .get('#search')
+      .click()
+      .get('#stockTable')
+      .find('tbody')
+      .find('tr')
+      .should('have.length', 17)
+  })
+
+  it('7. Enter Dolna 5a, radius = 1300 and test if 20 shops were returned', () => {
+    cy.get('#radius')
+      .clear()
+      .type(1300)
+      .get('#search')
+      .click()
+      .get('#stockTable')
+      .find('tbody')
+      .find('tr')
+      .should('have.length', 20)
+  })
+
+  it('8. Enter Dolna 5a, radius = 1400 and test if 20 shops were returned', () => {
+    cy.get('#radius')
+      .clear()
+      .type(1400)
+      .get('#search')
+      .click()
+      .get('#stockTable')
+      .find('tbody')
+      .find('tr')
+      .should('have.length', 20)
+  })
+
+  it('9. Enter Dolna 5a, radius = 1500 and test if 23 shops were returned', () => {
+    cy.get('#radius')
+      .clear()
+      .type(1500)
+      .get('#search')
+      .click()
+      .get('#stockTable')
+      .find('tbody')
+      .find('tr')
+      .should('have.length', 23)
+  })
+
+  it('10. Enter Dolna 5a, radius = 2000 and test if 35 shops were returned', () => {
+    cy.get('#radius')
+      .clear()
+      .type(2000)
+      .get('#search')
+      .click()
+      .get('#stockTable')
+      .find('tbody')
+      .find('tr')
+      .should('have.length', 35)
+  })
+
 })
 
 
